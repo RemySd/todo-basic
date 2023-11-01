9b2ff1121e9d4881f218c52e4fb374606e0f4bc2
Remy <remy.sainsard@gmail.com>
2023-11-01 14:25:03 +0100

    Masonry feature added

--- assets/app.js
+++ assets/app.js
@@ -2,51 +2,65 @@ import './styles/app.scss';
 
 require('bootstrap');
 
-const removeButtons = document.querySelectorAll('.remove-todo');
-
-removeButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const todoId = button.closest('.todo-item').getAttribute('data-todo-id');
-
-        fetch(
-            `/app/${todoId}/remove`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then((response) => {
-                return response.json();
-            }).then(function (data) {
-                button.closest('.todo-item').remove();
-            }).catch(function (error) {
-                console.log('something fail');
-            });
+document.addEventListener("DOMContentLoaded", function () {
+    const masonryContainer = document.querySelector('.todo-items');
+    const masonry = new Masonry(masonryContainer, {
+        percentPosition: true,
+        itemSelector: '.col'
     });
+
+    const removeButtons = document.querySelectorAll('.remove-todo');
+
+    removeButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const todoId = button.closest('.todo-item').getAttribute('data-todo-id');
+
+            fetch(
+                `/app/${todoId}/remove`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }).then((response) => {
+                    return response.json();
+                }).then(function (data) {
+                    button.closest('.todo-item').remove();
+                    masonry.reloadItems();
+                    masonry.layout();
+                }).catch(function (error) {
+                    console.log('something fail');
+                });
+        });
+    });
+
+    const checkButtons = document.querySelectorAll('.check-todo');
+
+    checkButtons.forEach((button) => {
+        button.addEventListener('click', () => {
+            const todoId = button.closest('.todo-item').getAttribute('data-todo-id');
+
+            fetch(
+                `/app/${todoId}/check`,
+                {
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json",
+                    },
+                }).then((response) => {
+                    return response.json();
+                }).then(function (data) {
+                    button.closest('.card').classList.remove('text-bg-light');
+                    button.closest('.card').classList.add('text-bg-success');
+                    button.remove();
+                }).catch(function (error) {
+                    console.log('something fail');
+                });
+        });
+    });
+
+
 });
 
-const checkButtons = document.querySelectorAll('.check-todo');
 
-checkButtons.forEach((button) => {
-    button.addEventListener('click', () => {
-        const todoId = button.closest('.todo-item').getAttribute('data-todo-id');
-
-        fetch(
-            `/app/${todoId}/check`,
-            {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            }).then((response) => {
-                return response.json();
-            }).then(function (data) {
-                button.closest('.card').classList.remove('text-bg-light');
-                button.closest('.card').classList.add('text-bg-success');
-                button.remove();
-            }).catch(function (error) {
-                console.log('something fail');
-            });
-    });
-});
 

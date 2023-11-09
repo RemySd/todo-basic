3351ceb4f281d03003e8eed23e78f8814e095ae4
Remy <remy.sainsard@gmail.com>
2023-11-09 13:42:30 +0100

    Fix minor bug

--- src/EventSubscriber/LocaleSubscriber.php
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -5,11 +5,12 @@ namespace App\EventSubscriber;
 use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 use Symfony\Contracts\Translation\TranslatorInterface;
+use Symfony\Component\Translation\DataCollectorTranslator;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 class LocaleSubscriber implements EventSubscriberInterface
 {
-    private TranslatorInterface $translator;
+    private DataCollectorTranslator $translator;
 
     /**
      * @required
@@ -17,12 +18,16 @@ class LocaleSubscriber implements EventSubscriberInterface
     public function setTranslator(TranslatorInterface $translator): void
     {
         $this->translator = $translator;
+        dump($this->translator);
     }
     
     public function onKernelRequest(RequestEvent $event): void
     {
         $request = $event->getRequest();
 
+        if (!$request->hasPreviousSession()) {
+            return;
+        }
 
         // try to see if the locale has been set as a _locale routing parameter
         if ($locale = $request->attributes->get('_locale')) {
@@ -30,7 +35,7 @@ class LocaleSubscriber implements EventSubscriberInterface
         } else {
             // if no explicit locale has been set on this request, use one from the session
             $request->setLocale($request->getSession()->get('_locale', 'en'));
-            $this->translator->setLocale($request->getSession()->get('_locale', 'en'));
+            $this->translator->setlocale($request->getSession()->get('_locale', 'en'));
         }
     }
 
